113d4d0767da0014cdab30383c355f45ae2015f6
Lakhan Singh <106170716+Lakhansi@users.noreply.github.com>
2025-09-21 06:26:52 +0530

    utils.h

--- utils.h
+++ utils.h
@@ -5,7 +5,7 @@
 #include <filesystem>
 #include <string>
 
-// DECLARATION ONLY - no implementation here
 std::string readFileContent(const std::filesystem::path& file_path);
 
-#endif // UTILS_H
+#endif 
+
